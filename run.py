89d021a216bb377866155189a3a986e53c88a631
mmblack4 <mm4.muthumani@gmail.com>
2020-04-25 19:46:57 +0530

    db and index issue fixed

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 from app import app
 from db import db
+from models.schema import *
 
 db.init_app(app)
 
 @app.before_first_request
 def create_tables():
     db.create_all()
+    if adminUser.query.filter_by(username="root").first() is None:
+        adminID = adminUser(username="root", password="root")
+        db.session.add(adminID)
+        db.session.commit()
