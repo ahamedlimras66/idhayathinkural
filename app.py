729d4c74ba869f3f5f0b64deeab0bfddcd582d7d
mmblack4 <mm4.muthumani@gmail.com>
2020-05-01 21:11:21 +0530

    password size incressed

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from flask_login import LoginManager, login_user, login_required, logout_user, c
 
 app = Flask(__name__,static_folder='static')
 app.secret_key = 'my-secret-key'
-app.config['UPLOAD_FOLDER'] = "file/req"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['UPLOAD_FOLDER'] = "file/req"
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
