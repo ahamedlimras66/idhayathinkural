4de96bf1e23a1495ed4ac038aa432b782de34507
mmblack4 <mm4.muthumani@gmail.com>
2020-05-01 20:47:12 +0530

    donate info, requirement info

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 import os
+import datetime
+import tempfile
 from models.schema import *
 from flask_admin import Admin, AdminIndexView
 from flask_admin.contrib.sqla import ModelView
 from werkzeug.security import check_password_hash, generate_password_hash
-from flask import Flask, render_template, request, url_for, redirect, send_from_directory
+from flask import Flask, render_template, request, url_for, redirect, send_from_directory, send_file
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__,static_folder='static')
 app.secret_key = 'my-secret-key'
+app.config['UPLOAD_FOLDER'] = "file/req"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 login_manager = LoginManager()
@@ -41,6 +44,11 @@ admin = Admin(app, index_view=MyAdminIndexView())
 admin.add_view(ModelView(commandBox,db.session))
 admin.add_view(UserAdmin(adminUser,db.session))
 admin.add_view(ModelView(event,db.session))
+admin.add_view(ModelView(donateDetails,db.session))
+admin.add_view(ModelView(requirementDetial,db.session))
+
+
+
 @app.route('/sitemap.xml')
 def static_sitemap():
     return send_from_directory(app.static_folder, request.path[1:])
@@ -70,7 +78,18 @@ def gal():
 
 @app.route('/events')
 def events():
-    return render_template('events.html')
+    cmds = commandBox.query.all()
+    events = event.query.first()
+    if events is not None:
+        events = str(events.dateTime)
+        return render_template('index.html',
+                                cmds=cmds,
+                                month=events[5:7],
+                                date=events[8:10],
+                                year=events[0:4],
+                                time=events[10:])
+    else:
+        return render_template('events.html',cmds=cmds)
 
 @app.route('/contact')
 def contact():
@@ -83,6 +102,44 @@ def Donate():
 @app.route('/Requirement')
 def Requirement():
     return render_template('Requirement.html')
+
+@app.route('/RequirementDetial', methods=['POST'])
+def RequirementDetial():
+    file = request.files['myfile']
+    fileformat = file.filename[file.filename.find('.'):]
+    if fileformat in ['.png', '.jpg', '.pdf', '.doc']:
+        name = request.form['name']
+        address = request.form['address']
+        requirement = request.form['requirement']
+        strength = request.form['strength']
+        mydate = datetime.datetime.strptime(request.form['mydate'], '%Y-%m-%d')
+        if name!='' and address!='' and strength!='':
+            req = requirementDetial(name=name,
+                                    address=address,
+                                    requirement=requirement,
+                                    strength=strength,
+                                    mydate=mydate)
+            db.session.add(req)
+            db.session.commit()
+            filename  = str(req.id)+fileformat
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            req.url = "http://www.idhayathinkural.in/file/"+filename
+            db.session.commit()
+            return render_template('Requirement.html',info="Thanks for filled")
+        else:
+            error="please enter Requirement corretal"
+    else:
+        error="please check file format (.png, .jpg, .pdf, .doc)"
+    return render_template('Requirement.html',error=error)
+
+@app.route("/file/<fileName>")
+@login_required
+def view(fileName):
+    print(fileName)
+    try:
+        return send_file('file/req/'+fileName, attachment_filename="test")
+    except:
+        pass
     
 @app.route('/report')
 def report():
@@ -96,6 +153,24 @@ def  submit():
     db.session.add(cmd)
     db.session.commit()
     return redirect('/')
+
+@app.route('/DonateDetails',methods=['POST'])
+def DonateDetails():
+    name = request.form['name']
+    phone = request.form['phoneno']
+    things = request.form['things']
+    mydate = datetime.datetime.strptime(request.form['mydate'], '%Y-%m-%d')
+    if name!='' and phone!='' and things!='title':
+        dd = donateDetails(name=name,
+                        phone=phone,
+                        things=things,
+                        mydate=mydate
+        )
+        db.session.add(dd)
+        db.session.commit()
+        return render_template('Donate.html',info="Thanks for Donate")
+    else:
+        return render_template('Donate.html',error="please enter details corretal")
 
 @app.route('/login')
 def login():
--- models/schema.py
+++ models/schema.py
@@ -16,3 +16,20 @@ class event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     event = db.Column(db.String(40),nullable=False)
     dateTime = db.Column(db.DateTime,nullable=False)
+
+class donateDetails(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(40),nullable=False)
+    phone = db.Column(db.Integer,nullable=False)
+    things = db.Column(db.String(40),nullable=False)
+    mydate = db.Column(db.Date,nullable=False)
+
+class requirementDetial(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(40),nullable=False)
+    address = db.Column(db.Integer,nullable=False)
+    requirement = db.Column(db.String(40))
+    strength = db.Column(db.String(40),nullable=False)
+    mydate = db.Column(db.Date,nullable=False)
+    url = db.Column(db.String(40))
+
